ee8705b727bdf848d86fe3ebb06f90f0c7643d5e
kodzyz <kodzyzdok@gmail.com>
2021-10-06 00:35:04 +0300

    task_2_2 ошибка: '5' не превращалось в '05' - исправил добавил elif int(s) < 10:

--- Daviduk_Kosta_dz_2/task_2_2.py
+++ Daviduk_Kosta_dz_2/task_2_2.py
@@ -10,11 +10,13 @@ while i < l:
     if '0' <= a[i] <= '9' or s[0] == '+':
         if s[0] == '+'and int(s) < 10:
             a[i] = '+' + '0' + s[1:]
+        elif int(s) < 10:
+            a[i] = '0' + s
         elif int(s) > 10:
             a[i] = s
         while 1:
-            a.insert(i, '  ')
-            a.insert(i+2, '  ')
+            a.insert(i, ' @ ')
+            a.insert(i+2, ' @ ')
             l = len(a)
             i += 2
             break
