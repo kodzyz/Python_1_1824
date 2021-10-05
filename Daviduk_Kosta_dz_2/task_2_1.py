accfb2e70ceb1167e353c9d7d1086b3d9b149c8a
kodzyz <kodzyzdok@gmail.com>
2021-10-06 00:26:55 +0300

    task_2_1 - сделал task_2_2 - обработал список in place

--- Daviduk_Kosta_dz_2/task_2_1.py
+++ Daviduk_Kosta_dz_2/task_2_1.py
@@ -1,1 +1,15 @@
-# урок 2 начало
+#2.1 Выяснить тип результата выражений:
+# 15 * 3
+# 15 / 3
+# 15 // 2
+# 15 ** 2
+
+a = 15 * 3
+print(type(a)) #<class 'int'>
+b = 15 / 3
+print(type(b)) #<class 'float'>
+print(isinstance(b, float)) #True
+c = 15 // 2
+print(type(c)) #<class 'int'>
+d = 15 ** 2
+print(type(d)) #<class 'int'>
